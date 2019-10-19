b33437d6e73bbbe0d5c33fe6a01c8b898bbf865d
jbzdarkid <jbzdarkid@gmail.com>
2019-10-19 13:37:15 -0700

    More notes

--- Main.cpp
+++ Main.cpp
@@ -78,11 +78,10 @@ llvm::Error ReadModuleInfo(const string& modulePath, ModuleInfo& info)
 // TODO: in64_t? How else do we work with 64 bit processes?
 cv::PublicSym32 CreatePublicSymbol(const char* name, int32_t offset) {
     using namespace llvm::codeview;
-
     PublicSym32 symbol(SymbolRecordKind::PublicSym32);
+    symbol.Flags = PublicSymFlags::Function;
     symbol.Offset = offset;
     symbol.Segment = 1;
-    symbol.Flags = PublicSymFlags::Function;
     symbol.Name = name;
     return symbol;
 }
@@ -125,9 +124,37 @@ void GeneratePDB(const ModuleInfo& moduleInfo, const vector<cv::PublicSym32>& pu
         {reinterpret_cast<uint8_t const*>(moduleInfo.sections.data()),
          moduleInfo.sections.size() * sizeof(moduleInfo.sections[0])}));
 
-    const char* moduleName = "d:\\dummy.obj";
+    const char* moduleName = "d:\\dummy.obj"; // Name doesn't actually matter, since there is no real object file.
     auto& modiBuilder = ExitOnErr(dbiBuilder.addModuleInfo(moduleName));
     modiBuilder.setObjFileName(moduleName);
+
+    // Add files to module (presumably necessary to associate source code lines)
+    for (auto file : {"main.c"})
+        ExitOnErr(dbiBuilder.addModuleSourceFile(modiBuilder, file));
+
+    // WIP: How do I make symbols? What even are symbols?
+    // S_BPREL16 and S_BPREL32 are how we name "Symbols RELative to the Base Pointer"
+    // See BPRelativeSym.
+    //  Offset: Signed offset relative to base pointer (oh, does this include args, too?)
+    //      If 0, variable was optimized away
+    //  @type: ???
+    //  name: Length-prefixed name of symbol
+    // S_LDATA32 - Static variables (?)
+    // S_LPROC32 - Local function (aka static or not-exported)
+    // S_GPROC32 - Global function (aka class member). I expect to use this one exclusively. It's hard/impossible to decide if a function is local or not.
+    //  Contains function length, as well as "when the stack is initialized", which seems helpful.
+    // S_LOCAL - Only defined by llvm, seems to refer to arguments?
+    vector<cv::CVSymbol> symbols;
+    for (auto symbol : symbols) {
+        modiBuilder.addSymbol(symbol);
+    }
+
+    // Apparently, I want a subsection of a module.
+    cv::DebugSubsectionKind::Lines;
+
+
+
+
 
     auto& gsiBuilder = builder.getGsiBuilder();
     for (const cv::PublicSym32& pub : publics)
@@ -135,6 +162,10 @@ void GeneratePDB(const ModuleInfo& moduleInfo, const vector<cv::PublicSym32>& pu
 
     auto& tpiBuilder = builder.getTpiBuilder();
     tpiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
+    // This is where type information goes, I think. Idk.
+    // cv::CVType type;
+    // tpiBuilder.addTypeRecord(type.RecordData, llvm::None);
+
 
     auto& ipiBuilder = builder.getIpiBuilder();
     ipiBuilder.setVersionHeader(llvm::pdb::PdbTpiV80);
@@ -148,8 +179,8 @@ void GeneratePDB(const ModuleInfo& moduleInfo, const vector<cv::PublicSym32>& pu
     // dbiBuilder.setGlobalsStreamIndex(gsiBuilder.getGlobalsStreamIndex());
     // dbiBuilder.setSymbolRecordStreamIndex(gsiBuilder.getRecordStreamIdx());
 
-    auto mutableGuid = moduleInfo.guid;
-    ExitOnErr(builder.commit(outputFileName, &mutableGuid));
+    cv::GUID ignoredOutGuid;
+    ExitOnErr(builder.commit(outputFileName, &ignoredOutGuid));
 }
 
 } // namespace
