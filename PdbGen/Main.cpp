8acd41385ffd61b0cbe3516ad4f311be7a466ead
jbzdarkid <jbzdarkid@gmail.com>
2019-11-01 17:58:38 -0700

    Attempt failed

--- PdbGen/Main.cpp
+++ PdbGen/Main.cpp
@@ -87,9 +87,6 @@ void GeneratePDB(char const* outputPDB)
     ModuleInfo moduleInfo = ReadModuleInfo("C:/Users/localhost/Documents/GitHub/PdbGen/PdbTest/Debug/PdbTest.exe");
 
     const char* filename = "C:/Users/localhost/Documents/Github/PdbGen/Generated/Main.cpp";
-    // Base address is 0x4F1000
-    int32_t funcStart = 0x10; // Offset from the base address
-    int32_t funcEnd = 59; // Function length (Total instruction count, including ret)
 
     PDBFileBuilder builder(llvmAllocator);
     ExitOnErr(builder.initialize(4096)); // Blocksize
@@ -109,90 +106,195 @@ void GeneratePDB(char const* outputPDB)
     dbiBuilder.setVersionHeader(PdbDbiV70);
 
     ExitOnErr(dbiBuilder.addModuleInfo("* Linker Generated Manifest RES *"));
-    SectionContrib sc{};
-    sc.Imod = 1;
-    sc.ISect = 1;
-    sc.Off = funcStart;
-    sc.Size = funcEnd;
-    sc.Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_ALIGN_16BYTES | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ;
-    dbiBuilder.addSectionContrib(sc);
 
     DebugStringTableSubsection strings; // Declared outside because this object crashes during the destructor
+    auto checksums = std::make_shared<DebugChecksumsSubsection>(strings);
+
+    DbiModuleDescriptorBuilder& module = ExitOnErr(dbiBuilder.addModuleInfo("D:/dummy.obj"));
+    module.setObjFileName("D:/dummy.obj");
+    ExitOnErr(dbiBuilder.addModuleSourceFile(module, filename));
+
+    int FD;
+    if (auto ec = openFileForRead(filename, FD, OpenFlags::OF_None))
+        ExitOnErr(llvm::errorCodeToError(ec));
+    auto result = md5_contents(FD);
+    if (!result)
+        ExitOnErr(llvm::errorCodeToError(result.getError()));
+    checksums->addChecksum(filename, FileChecksumKind::MD5, result.get().Bytes);
+    module.addDebugSubsection(checksums);
 
     {
-        DbiModuleDescriptorBuilder& module = ExitOnErr(dbiBuilder.addModuleInfo("D:/dummy.obj"));
-        module.setObjFileName("D:/dummy.obj");
-        ExitOnErr(dbiBuilder.addModuleSourceFile(module, filename));
+        auto sym = ObjNameSym(SymbolRecordKind::ObjNameSym);
+        sym.Name = "C:/Users/localhost/Documents/GitHub/PdbGen/PdbTest/Debug/asdf.obj"; // semi-immutable for some awful reason
+        AddSymbol(module, sym);
+    }
+    {
+        // The backend version must be a valid MSVC version. See LLD documentation:
+        // https://github.com/llvm-mirror/lld/blob/master/COFF/PDB.cpp#L1395
+        auto cs = Compile3Sym(SymbolRecordKind::Compile3Sym);
+        cs.VersionBackendMajor = 19;
+        cs.VersionBackendMinor = 23;
+        cs.VersionBackendBuild = 28016;
+        cs.VersionBackendQFE = 4;
+        cs.Version = "Microsoft (R) Optimizing Compiler";
+        cs.setLanguage(SourceLanguage::Cpp);
+        AddSymbol(module, cs);
+    }
+    {
+        auto sym = UsingNamespaceSym(SymbolRecordKind::UsingNamespaceSym);
+        AddSymbol(module, sym);
+    }
 
-        auto checksums = std::make_shared<DebugChecksumsSubsection>(strings);
-        int FD;
-        if (auto ec = openFileForRead(filename, FD, OpenFlags::OF_None))
-            ExitOnErr(llvm::errorCodeToError(ec));
-        auto result = md5_contents(FD);
-        if (!result)
-            ExitOnErr(llvm::errorCodeToError(result.getError()));
-        checksums->addChecksum(filename, FileChecksumKind::MD5, result.get().Bytes);
-        module.addDebugSubsection(checksums);
+    GSIStreamBuilder& gsiBuilder = builder.getGsiBuilder();
 
-        auto debugSubsection = std::make_shared<DebugLinesSubsection>(*checksums, strings);
-        debugSubsection->createBlock(filename);
-        debugSubsection->setCodeSize(funcEnd);
-        debugSubsection->setRelocationAddress(1, funcStart);
-        debugSubsection->setFlags(LineFlags::LF_None);
+    {
+        // Base address is 0x4F1000
+        int32_t funcStart = 0x20; // Offset from the base address
+        int32_t funcLen = 48; // Function length (Total instruction count, including ret)
 
-        debugSubsection->addLineInfo(0, LineInfo(1, 1, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(4, LineInfo(2, 2, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(11, LineInfo(3, 3, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(20, LineInfo(4, 4, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(28, LineInfo(5, 5, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(37, LineInfo(6, 6, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(52, LineInfo(7, 7, true)); // Offset, Start, End, isStatement
-        debugSubsection->addLineInfo(55, LineInfo(8, 8, true)); // Offset, Start, End, isStatement
-        module.addDebugSubsection(debugSubsection);
+        // SectionContrib sc{};
+        // sc.Imod = 1;
+        // sc.ISect = 1;
+        // sc.Off = funcStart;
+        // sc.Size = funcLen;
+        // sc.Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_ALIGN_16BYTES | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ;
+        // dbiBuilder.addSectionContrib(sc);
 
         {
-            auto sym = ObjNameSym(SymbolRecordKind::ObjNameSym);
-            sym.Name = "C:/Users/localhost/Documents/GitHub/PdbGen/PdbTest/Debug/asdf.obj"; // semi-immutable for some awful reason
-            AddSymbol(module, sym);
+            auto debugSubsection = std::make_shared<DebugLinesSubsection>(*checksums, strings);
+            debugSubsection->createBlock(filename);
+            debugSubsection->setCodeSize(funcLen);
+            debugSubsection->setRelocationAddress(1, funcStart);
+            debugSubsection->setFlags(LineFlags::LF_None);
+
+            debugSubsection->addLineInfo(0x00, LineInfo(6, 6, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x03, LineInfo(7, 7, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x0C, LineInfo(8, 8, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x13, LineInfo(9, 9, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x1C, LineInfo(10, 10, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x2B, LineInfo(11, 11, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(0x2E, LineInfo(12, 12, true)); // Offset, Start, End, isStatement
+            module.addDebugSubsection(debugSubsection);
+
+            {
+                auto sym = ProcSym(SymbolRecordKind::GlobalProcSym);
+                sym.Parent = 0;
+                sym.End = 240;
+                sym.Next = 0;
+                sym.CodeSize = funcLen;
+                sym.CodeOffset = funcStart;
+                sym.Segment = 1;
+                sym.Name = "foo";
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = FrameProcSym(SymbolRecordKind::FrameProcSym);
+                sym.TotalFrameBytes = 4;
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = BPRelativeSym(SymbolRecordKind::BPRelativeSym);
+                sym.Offset = -4;
+                sym.Type = TypeIndex(SimpleTypeKind::Int16); // Mutable!
+                sym.Name = "q"; // Mutable!
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = ScopeEndSym(SymbolRecordKind::ScopeEndSym);
+                AddSymbol(module, sym);
+            }
+        }
+
+        {
+            ProcRefSym sym(SymbolRecordKind::ProcRefSym);
+            sym.Module = 2;
+            sym.Name = "foo";
+            sym.SymOffset = 148; // Symbol offset in the module of the S_GPROC32.
+            gsiBuilder.addGlobalSymbol(sym);
         }
         {
-            // The backend version must be a valid MSVC version. See LLD documentation:
-            // https://github.com/llvm-mirror/lld/blob/master/COFF/PDB.cpp#L1395
-            auto cs = Compile3Sym(SymbolRecordKind::Compile3Sym);
-            cs.VersionBackendMajor = 19;
-            cs.VersionBackendMinor = 23;
-            cs.VersionBackendBuild = 28016;
-            cs.VersionBackendQFE = 4;
-            cs.Version = "Microsoft (R) Optimizing Compiler";
-            AddSymbol(module, cs);
-        }
-        {
-            auto sym = UsingNamespaceSym(SymbolRecordKind::UsingNamespaceSym);
-            // sym.Name = "std";
-            AddSymbol(module, sym);
-        }
-        {
-            auto sym = ProcSym(SymbolRecordKind::GlobalProcSym);
-            sym.Parent = 0;
-            sym.End = 240;
-            sym.Next = 0;
-            sym.CodeSize = funcEnd;
-            sym.CodeOffset = funcStart;
+            PublicSym32 sym(SymbolRecordKind::PublicSym32);
+            sym.Flags = PublicSymFlags::Function;
+            sym.Offset = funcStart;
             sym.Segment = 1;
-            sym.Name = "main"; // Immutable -- maybe because this is an entry point?
-            AddSymbol(module, sym);
+            sym.Name = "?foo@@YAHH@Z";
+            gsiBuilder.addPublicSymbol(sym);
+        }
+    }
+
+    {
+        // Base address is 0x4F1000
+        int32_t funcStart = 0x50; // Offset from the base address
+        int32_t funcLen = 59; // Function length (Total instruction count, including ret)
+
+        SectionContrib sc{};
+        sc.Imod = 1;
+        sc.ISect = 1;
+        sc.Off = 0x20;
+        sc.Size = 107;
+        sc.Characteristics = IMAGE_SCN_CNT_CODE | IMAGE_SCN_ALIGN_16BYTES | IMAGE_SCN_MEM_EXECUTE | IMAGE_SCN_MEM_READ;
+        dbiBuilder.addSectionContrib(sc);
+
+        {
+            auto debugSubsection = std::make_shared<DebugLinesSubsection>(*checksums, strings);
+            debugSubsection->createBlock(filename);
+            debugSubsection->setCodeSize(funcLen);
+            debugSubsection->setRelocationAddress(1, funcStart);
+            debugSubsection->setFlags(LineFlags::LF_None);
+
+            debugSubsection->addLineInfo(0, LineInfo(10, 10, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(4, LineInfo(11, 11, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(11, LineInfo(12, 12, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(20, LineInfo(13, 13, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(28, LineInfo(14, 14, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(37, LineInfo(15, 15, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(52, LineInfo(16, 16, true)); // Offset, Start, End, isStatement
+            debugSubsection->addLineInfo(55, LineInfo(17, 17, true)); // Offset, Start, End, isStatement
+            module.addDebugSubsection(debugSubsection);
+
+            {
+                auto sym = ProcSym(SymbolRecordKind::GlobalProcSym);
+                sym.Parent = 0;
+                sym.End = 240;
+                sym.Next = 0;
+                sym.CodeSize = funcLen;
+                sym.CodeOffset = funcStart;
+                sym.Segment = 1;
+                sym.Name = "main"; // Immutable -- maybe because this is an entry point?
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = FrameProcSym(SymbolRecordKind::FrameProcSym);
+                sym.TotalFrameBytes = 4;
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = BPRelativeSym(SymbolRecordKind::BPRelativeSym);
+                sym.Offset = -4;
+                sym.Type = TypeIndex(SimpleTypeKind::Int16); // Mutable!
+                sym.Name = "b"; // Mutable!
+                AddSymbol(module, sym);
+            }
+            {
+                auto sym = ScopeEndSym(SymbolRecordKind::ScopeEndSym);
+                AddSymbol(module, sym);
+            }
+        }
+
+        {
+            ProcRefSym sym(SymbolRecordKind::ProcRefSym);
+            sym.Module = 2;
+            sym.Name = "main";
+            sym.SymOffset = 244;
+            gsiBuilder.addGlobalSymbol(sym);
         }
         {
-            auto sym = BPRelativeSym(SymbolRecordKind::BPRelativeSym);
-            sym.Offset = -4;
-            sym.Type = TypeIndex(SimpleTypeKind::Int16); // Mutable!
-            sym.Name = "b"; // Mutable!
-            AddSymbol(module, sym);
-        }
-        {
-            auto sym = FrameProcSym(SymbolRecordKind::FrameProcSym);
-            sym.TotalFrameBytes = 4;
-            AddSymbol(module, sym);
+            PublicSym32 sym(SymbolRecordKind::PublicSym32);
+            sym.Flags = PublicSymFlags::Function;
+            sym.Offset = funcStart;
+            sym.Segment = 1;
+            sym.Name = "_main"; // Immutable -- decorated name
+            gsiBuilder.addPublicSymbol(sym);
         }
     }
 
@@ -200,16 +302,6 @@ void GeneratePDB(char const* outputPDB)
         reinterpret_cast<const uint8_t*>(moduleInfo.sections.data()),
         moduleInfo.sections.size() * sizeof(moduleInfo.sections[0])
     }));
-
-    GSIStreamBuilder& gsiBuilder = builder.getGsiBuilder();
-    {
-        PublicSym32 sym(SymbolRecordKind::PublicSym32);
-        sym.Flags = PublicSymFlags::Function;
-        sym.Offset = funcStart;
-        sym.Segment = 1;
-        sym.Name = "_main";
-        gsiBuilder.addPublicSymbol(sym);
-    }
 
     // The TPI and IPI streams aren't strictly necessary... but pdbutil fails if they're not present.
     TpiStreamBuilder& tpiBuilder = builder.getTpiBuilder();
